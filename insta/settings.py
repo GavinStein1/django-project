c2592e0f1c58ad99bf2ae62ad860727c8311e703
Gavin Stein <gste6115@uni.sydney.edu.au>
2022-08-23 10:06:08 +1000

    change static settings

--- insta/settings.py
+++ insta/settings.py
@@ -136,9 +136,9 @@ LOGOUT_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/var/www/gallerio.xyz/static"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
